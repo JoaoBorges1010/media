69f773328904f3d2e1888598ae3d21dcd538ac77
joaoborges <joaoborgesbiz@gmail.com>
2023-04-22 21:35:08 +0100

    Photos feature using redux toolkit query and Tags for automatic fetching

--- src/components/AlbumsListitem.js
+++ src/components/AlbumsListitem.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import ExpandablePanel from "./ExpandablePanel";
 import { GoTrashcan } from "react-icons/go";
 import { useRemoveAlbumMutation } from "../store";
+import PhotosList from "./PhotosList";
 
 
 function AlbumsListItem({ album }) {
@@ -19,7 +20,7 @@ function AlbumsListItem({ album }) {
     </>
 
   return <ExpandablePanel key={album.id} header={header}>
-    List of Photos in the Album
+    <PhotosList album={album} />
   </ExpandablePanel>
 };
 
